44e018012237358bb83f7f6eed40ebab3bc831a2
introtoxicXXl <minhajulabedin648@gmail.com>
2022-08-01 23:38:49 +0600

    card edit,pic edit

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import './Card.css'
 
 const Card = ({ data }) => {
   const { EName, img } = data;
   return (
     
-      <div className="col rounded">
-        <div className="card">
+      <div className="col">
+        <div className="card eCard h-100">
           <img src={img} className="card-img-top" alt="" />
           <div className="card-body">
-            <h5 className="card-title">{EName}</h5>
+            <h5 className="card-title text-center">{EName}</h5>
           </div>
         </div>
       </div>
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -9,8 +9,15 @@ const Home = () => {
       .then(data => setDatas(data))
   }, [])
   return (
-    <div className='container'>
-      <div className='row row-cols-1 row-cols-md-3 g-3'>
+    <div className='container mb-5'>
+      <div>
+        <h2 className='text-center fw-bold mt-5 mb-4'>I grow by helping people in need.</h2>
+        <div className="input-group w-25 mb-4 m-auto">
+          <input type="text" className="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2"/>
+            <button className="btn btn-primary" type="button" id="button-addon2">Search</button>
+        </div>
+      </div>
+      <div className='row row-cols-1 row-cols-md-4 g-3'>
         {
           datas.map(data => <Card
             data={data}
